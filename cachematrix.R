5608e6e00d50acc6505dbb26619153c1826f5741
NguyenHongNhung <nnhung2@gmail.com>
2016-07-19 11:28:13 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,15 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix will return a list of four functions: set, get, setsolve, getsolve.
+## At first, user should set a matrix by using the function set. For example:
+## matrixx <- matrix (c(1,0,5,2,1,6,3,4,0), 3, 3)
+## mcm <- makeCacheMatrix()
+## mcm$set(matrixx)
 
-## Write a short comment describing this function
+## Then if we call the function get, we will retrive the matrix that was set before.
+## If we don't compute the inversed matrix, when we call mcm$getsolve(), NULL will be
+## returned. 
+
+## Note that we can set an wrong inversed matrix by using mcm$setsolve(). In this case,
+## when we use cacheSolve later, the wrong inversed matrix will be returned.
 
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
@@ -18,7 +26,10 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## The functin cacheSolve returns an inversed matrix that was set in makeCacheMatrix before
+## If there is no inversed matrix was stored in the makeCacheMatrix, cacheSolve
+## will compute and return an inversed matrix, as well as store it in the makeCacheMatrix
+## where we can retrive it later by using: makeCacheMatrix$getsolve()
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
@@ -28,7 +39,7 @@ cacheSolve <- function(x, ...) {
     return(m)
   }
   data <- x$get()
-  m <- mean(data, ...)
+  m <- solve (data, ...)
   x$setsolve(m)
   m
 }
